8ab70fd85530b445b18e4f86dbf7c27136f83b9e
Darwin Ding <dingdarwin@gmail.com>
2020-01-10 23:06:46 -0500

    Habit Entries are now cyclical

--- web-app/src/components/Habit.js
+++ web-app/src/components/Habit.js
@@ -2,61 +2,94 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { syncScroll } from '../utils/habits.utils';
 
+const states = ["", "x", "X", "o", "O"];
+
 class HabitEntry extends Component {
     constructor(props) {
         super(props);
         if (props.entry) {
+            let entry = props.entry;
+            if (entry === true || entry === "true") {
+                entry = "x";    // fixing old entries
+            } else if (entry === false || entry === "false") {
+                entry = "";
+            }
             this.state = {
-                entry: props.entry
+                entry: entry
             };
         } else {
             this.state = {
-                entry: false
+                entry: ""
             };
         }
     }
 
     postUpdate(habit, user, day) {
-        const entry = this.refs.checkbox.checked;
-        const data = {
-            entry: entry,
-            date: day.format('MM/DD/YYYY'),
-            note: ""
-        };
-        
-        console.log("Posting update for " + habit + " on " + data.date + ", entry: " + data.entry);
-        axios.post('http://localhost:8082/api/users/' + user + '/habit/' + habit + '/entries', data)
-            .then(res => {
-                this.setState({
-                    ...this.state,
-                    entry: data.entry
-                });
-                console.log("Update posted successfully");
-                console.log(res);   
-            }).catch(err => {
-                console.log("Update had an error");
-                console.log(err);
-            })
+        let stateIndex = states.indexOf(this.state.entry);
+        if (stateIndex === states.length - 1) {
+            stateIndex = -1;
+        }
+
+        this.setState({
+            ...this.state,
+            entry: states[stateIndex + 1]
+        }, () => {
+            const entry = this.state.entry;
+            const data = {
+                entry: entry,
+                date: day.format('MM/DD/YYYY'),
+                note: ""
+            };
+            
+            console.log("Posting update for " + habit + " on " + data.date + ", entry: " + data.entry);
+            axios.post('http://localhost:8082/api/users/' + user + '/habit/' + habit + '/entries', data)
+                .then((res) => {
+                    console.log("Update posted successfully");
+                    console.log(res);   
+                }).catch(err => {
+                    console.log("Update had an error");
+                    console.log(err);
+                })
+        });
     }
 
-    checkboxIsChecked() {
-        if (this.state.entry === true || this.state.entry === "true") {
-            return true;
-        } else {
-            return false;
-        }
-    }
+    getClassName() {
+            }
 
     render() {
+        let entryDisplay = this.state.entry || "";
+
+        let className = "";
+        switch (this.state.entry) {
+            case "":
+                className = "habit-entry neutral";
+                break;
+            case "x":
+                className = "habit-entry positive";
+                break;
+            case "X":
+                className = "habit-entry very-positive";
+                break;
+            case "o":
+                className = "habit-entry negative";
+                break;
+            case "O":
+                className = "habit-entry very-negative";
+                break;
+            default:
+                className = "habit-entry neutral";
+                break;
+        }
+
         return (
             <div className="ctr-entry habit">
-                <input type="checkbox" ref="checkbox" checked={this.checkboxIsChecked()} onChange={() => this.postUpdate(this.props.habit, this.props.user, this.props.day)} />
+                <div className={className} onClick={() => this.postUpdate(this.props.habit, this.props.user, this.props.day)}>{entryDisplay}</div>
             </div>
         );
     }
 };
 
-class Habit extends Component {
+class Habit extends Component { 
     processEntries(entries) {
         let map = {};
 
